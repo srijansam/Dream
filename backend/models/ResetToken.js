6808e5f7a40fbae572c5987aa4a8d1b63ac5ceb8
srijansam <srijan.kumar2106@gmail.com>
2025-03-18 16:03:44 +0530

    reverted all the swtupid changes

--- backend/models/ResetToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const resetTokenSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 3600 } // Token expires after 1 hour
-});
-
-module.exports = mongoose.model("ResetToken", resetTokenSchema); 
